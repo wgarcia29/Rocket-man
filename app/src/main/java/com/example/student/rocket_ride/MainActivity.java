7af2c2b19d7f04d2902868a1ac22e527f928f423
student <student@iMac-10.local>
2018-11-01 18:56:16 -0700

    ehh

--- app/src/main/java/com/example/student/rocket_ride/MainActivity.java
+++ app/src/main/java/com/example/student/rocket_ride/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.student.rocket_ride;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;  // Jonathan
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
     }
 
     /**
